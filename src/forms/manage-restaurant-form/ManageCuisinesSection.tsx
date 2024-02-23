41a334a7393ea9e7c026213a5a7fe23e7bd591b1
devonmartindale <devonmartindale@gmail.com>
2024-02-23 13:37:22 +0200

    working on manageRestaurant UI

---
+++ src/forms/manage-restaurant-form/ManageCuisinesSection.tsx
@@ -0,0 +1,5 @@
+const ManageCuisinesSection = () => {
+  return <div>ManageCuisinesSection</div>;
+};
+
+export default ManageCuisinesSection;
---
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -0,0 +1,73 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/ui/form";
+import DetailsSection from "./DetailsSection";
+import { Separator } from "@/components/ui/separator";
+import CuisinesSection from "./CuisinesSection";
+
+const formSchema = z.object({
+  restaurantName: z.string({
+    required_error: "Restaurant name is required",
+  }),
+  city: z.string({
+    required_error: "City is required",
+  }),
+  country: z.string({
+    required_error: "Country is required",
+  }),
+  deliveryPrice: z.coerce.number({
+    required_error: "Delivery price is required",
+    invalid_type_error: "Delivery price must be a number",
+  }),
+  estimatedDeliveryTime: z.coerce.number({
+    required_error: "Delivery time is required",
+    invalid_type_error: "Delivery time must be a number",
+  }),
+  cuisines: z.array(z.string()).nonempty({
+    message: "Please select at least one item",
+  }),
+  menuItems: z.array(
+    z.object({
+      name: z.string().min(1, "name is required"),
+      price: z.coerce.number().min(1, "price is required"),
+    })
+  ),
+  imageFile: z.instanceof(File, { message: "Image is required" }),
+});
+
+type restaurantFormData = z.infer<typeof formSchema>;
+
+type Props = {
+  onSave: (restaurantFormData: FormData) => void;
+  isLoading: boolean;
+};
+
+const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
+  const form = useForm<restaurantFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      cuisines: [],
+      menuItems: [{ name: "", price: 0 }],
+    },
+  });
+
+  const onSubmit = (formData: restaurantFormData) => {
+    // TODO - convert formDatajson to a new FormData object
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-gray-50 p-10 rounded-lg"
+      >
+        <DetailsSection />
+        <Separator />
+        <CuisinesSection />
+      </form>
+    </Form>
+  );
+};
+
+export default ManageRestaurantForm;
