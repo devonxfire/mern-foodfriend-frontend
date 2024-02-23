f38d5db76b1fa9afb446f3ab6b603e5b02b6f982
devonmartindale <devonmartindale@gmail.com>
2024-02-23 16:24:16 +0200

    completed add restaaurant functionality

---
+++ src/api/MyRestaurantApi.tsx
@@ -0,0 +1,48 @@
+import { Restaurant } from "@/type";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useMutation } from "react-query";
+import { toast } from "sonner";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useCreateMyRestaurant = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const createMyRestaurantRequest = async (
+    restaurantFormData: FormData
+  ): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: restaurantFormData,
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Failed to create restaurant");
+    }
+
+    return response.json();
+  };
+
+  const {
+    mutate: createRestaurant,
+    isLoading,
+    isSuccess,
+    error,
+  } = useMutation(createMyRestaurantRequest);
+
+  if (isSuccess) {
+    toast.success("Restaurant created successfully!");
+  }
+
+  if (error) {
+    toast.error(error.message);
+  }
+
+  return { createRestaurant, isLoading };
+};
