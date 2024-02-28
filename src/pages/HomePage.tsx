247c46d67a9370d16d08480963fc0f0300f92d94
devonmartindale <devonmartindale@gmail.com>
2024-02-28 14:54:49 +0200

    create search ui and functionality

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,29 @@
 import landing from "../assets/landing.png";
 import appDownload from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
   return (
     <div className="flex flex-col gap-12">
-      <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gsp-5 text-center -mt-16">
+      <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gsp-5 text-center -mt-16 md:px-32">
         <h1 className="text-5xl font-bold tracking-tight text-orange-600">
           Tuck into a takeaway today
         </h1>
         <span className="text-lg pt-4 text-slate-500  ">
           Food is just a click away!
         </span>
+        <SearchBar
+          placeholder="Search by City or Town..."
+          onSubmit={handleSearchSubmit}
+        />
       </div>
 
       <div className="grid md:grid-cols-2 gap-5 ">
